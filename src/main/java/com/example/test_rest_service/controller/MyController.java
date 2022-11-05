fff283b7f122327bd84a9b4c76a8c8c17638837c
igoldinay <y_igoldina@mail.ru>
2022-11-05 19:37:55 +0500

    lab  3 done

--- src/main/java/com/example/test_rest_service/controller/MyController.java
+++ src/main/java/com/example/test_rest_service/controller/MyController.java
@@ -2,6 +2,7 @@ package com.example.test_rest_service.controller;
 
 import com.example.test_rest_service.model.Request;
 import com.example.test_rest_service.model.Response;
+import com.example.test_rest_service.service.ModifyRequestService;
 import com.example.test_rest_service.service.MyModifyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     private MyModifyService myModifyService;
+    private ModifyRequestService modifyRequestService;
 
     @Autowired
-    public MyController(@Qualifier("ModifySystemTime") MyModifyService myModifyService) {
+    public MyController(@Qualifier("ModifySystemTime") MyModifyService myModifyService, ModifyRequestService modifyRequestService) {
         this.myModifyService = myModifyService;
+        this.modifyRequestService = modifyRequestService;
     }
 
     @PostMapping(value = "/feedback")
@@ -35,6 +38,7 @@ public class MyController {
                 .errorMessage("234")
                 .build();
 
+        modifyRequestService.modifyRq(request);
         Response modifiedResponse = myModifyService.modify(response);
         log.info("Outgoing request:" + modifiedResponse);
         return new ResponseEntity<Response>(modifiedResponse, HttpStatus.OK);
